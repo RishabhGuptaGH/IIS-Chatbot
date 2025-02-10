3b6d8a46a62364d864dc570ac18d58772147db0a
RishabhGuptaGH <Rishabh24461@iiitd.ac.in>
2025-02-11 00:04:58 +0530

    Update normalizer.py
    made it more intutive and simpler

--- Chatbot/normalizer.py
+++ Chatbot/normalizer.py
@@ -1,36 +1,16 @@
-def textNormalizer(userInput): 
-    def wordNormalizer(word):
-        word = word.lower()
-        word = list(word)
-        validLetters = [chr(i) for i in range(97,133)]
-        validLetters.append("-")
-        validLetters.append("'")
-        validLetters.append("’")
-        newWordList = [i for i in word if i in validLetters]
-        return "".join(newWordList)
+def text_normalizer(user_input):
+    def word_normalizer(word):
+        valid_letters = set(chr(i) for i in range(97, 123)) | {"-", "'", "’"}
+        return "".join(c for c in word.lower() if c in valid_letters)
 
-    def wordStorage(data):
-        normalizedWordList = []
-        words = data.split()
-        for word in words:
-            word = wordNormalizer(word)
-            normalizedWordList.append(word)
-        return normalizedWordList
+    def word_storage(data):
+        return [word_normalizer(word) for word in data.split()]
     
-    def combinationOfWords(inputList):
-        newList = inputList.copy()
-        
-        for terms in range(2,5):
-            i = 0
-            while True:
-                try:
-                    combinedWord = inputList[i] + " "
-                    for j in range(1,terms):
-                        combinedWord += inputList[i + j]+" "
-                    newList.append(combinedWord.strip())
-                    i += 1
-                except:
-                    break
-        return newList
+    def combination_of_words(input_list):
+        new_list = input_list[:]
+        for terms in range(2, 5):
+            for i in range(len(input_list) - terms + 1):
+                new_list.append(" ".join(input_list[i:i+terms]))
+        return new_list
     
-    return combinationOfWords(wordStorage(userInput))
+    return combination_of_words(word_storage(user_input))
